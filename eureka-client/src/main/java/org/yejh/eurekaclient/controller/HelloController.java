61984a7748756058327931c9afffa737af5a1a3a
yejiahao <454696086@qq.com>
2019-03-31 20:37:36 +0800

    :tada: Initialize Repo

--- eureka-client/src/main/java/org/yejh/eurekaclient/controller/HelloController.java
+++ eureka-client/src/main/java/org/yejh/eurekaclient/controller/HelloController.java
@@ -1,6 +1,7 @@
 package org.yejh.eurekaclient.controller;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -8,17 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class HelloController {
-    private final Logger logger = Logger.getLogger(getClass());
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private DiscoveryClient client;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String index() {
-        ServiceInstance instance = client.getLocalServiceInstance();
-        logger.info("/hello, host: " + instance.getHost() + ", service id: " + instance.getServiceId());
-        return "Hello World";
+        String serviceId = System.getProperty("spring.application.name", "eureka-client-1");
+        List<ServiceInstance> instances = client.getInstances(serviceId);
+        for (ServiceInstance instance : instances) {
+            logger.info("/hello, host: {}, service id: {}", instance.getHost(), instance.getServiceId());
+        }
+        return "Hello " + serviceId;
     }
 }
